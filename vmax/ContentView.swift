6ef0f09dd57b231a7faac1c5341fda76fd92c444
Jared Grimes <jaredlgrimes@hotmail.com>
2019-12-20 14:46:51 -0500

    Initial Commit

---
+++ vmax/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vmax
+//
+//  Created by Jared Grimes on 12/20/19.
+//  Copyright © 2019 Jared Grimes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
